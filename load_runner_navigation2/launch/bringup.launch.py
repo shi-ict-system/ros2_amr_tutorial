f9f0e028302367172dc816856b3d1375f75fdd60
Byeong-Kyu Ahn <byeongkyu@gmail.com>
2021-11-23 21:11:39 +0900

    Add keepout filter

--- load_runner_navigation2/launch/bringup.launch.py
+++ load_runner_navigation2/launch/bringup.launch.py
@@ -40,6 +40,7 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
+        default_value=os.path.join(bringup_dir, 'maps', 'map_1637303820.yaml'),
         description='Full path to map yaml file to load')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
